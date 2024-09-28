6e7386568710bdce2e4218733fb02d65bdd5bf71
Ahmad Faiz Agustianto <107536877+Mifaki@users.noreply.github.com>
2024-09-28 20:45:10 +0700

    `chore(Metadata)`: Add useGenerateMetada

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,24 @@
-import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
 import "./globals.css";
+import { Poppins } from "next/font/google";
+import { useGenerateMetadata } from "@/shared/usecase/useGenerateMetada";
+import type { Metadata } from "next";
 
 const poppins = Poppins({
   subsets: ['latin'],
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
 })
 
-export const metadata: Metadata = {
-  title: "Ahmad Faiz Agustianto",
-  description: "Ahmad Faiz Agustianto's Portofolio",
-};
+export const metadata: Metadata = useGenerateMetadata({
+  pageTitle: "Home",
+  description: "Ahmad Faiz's portfolio",
+  canonicalPath: "/",
+  keywords: [
+    "Ahmad Faiz Agustianto",
+    "Ahmad Faiz",
+    "Faiz"
+  ],
+  type: "website",
+});
 
 export default function RootLayout({
   children,
