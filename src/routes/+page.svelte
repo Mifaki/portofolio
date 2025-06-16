5ba23e9e9778128ed8f67642e8123d00d5c67417
Mifaki <agustianto.d19@gmail.com>
2025-06-16 22:02:58 +0700

    feat: add custom fonts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,19 @@
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>Ahmad Faiz</title>
+	<meta name="description" content="Ahmad Faiz Portofolio" />
 </svelte:head>
 
 <section>
-	<h1>Under Construction</h1>
-	<h2 class="text-center">This whole website is being rebuilded by Faiz using svelte, <br />if you were given this link for faiz's portolio please use the link below</h2>
-	<a href="https://archived.ahmad-faiz.my.id" target="_blank" class="px-3 py-2 !text-white bg-[#ff3e00] rounded-sm mt-4">Go to my portfolio</a>
+	<h1 class="font-satoshi text-4xl font-bold">Under Construction</h1>
+	<h2 class="text-center">
+		This whole website is being rebuilded by Faiz using svelte, <br />if you were given this link
+		for faiz's portolio please use the link below
+	</h2>
+	<a
+		href="https://archived.ahmad-faiz.my.id"
+		target="_blank"
+		class="mt-4 rounded-sm bg-[#ff3e00] px-3 py-2 !text-white">Go to my portfolio</a
+	>
 </section>
 
 <style>
---
+++ tailwind.config.ts
@@ -0,0 +1,38 @@
+import type { Config } from 'tailwindcss';
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: [
+					'Inter',
+					'system-ui',
+					'-apple-system',
+					'BlinkMacSystemFont',
+					'Segoe UI',
+					'Roboto',
+					'Helvetica Neue',
+					'Arial',
+					'sans-serif'
+				],
+				satoshi: [
+					'Satoshi',
+					'Inter',
+					'system-ui',
+					'-apple-system',
+					'BlinkMacSystemFont',
+					'Segoe UI',
+					'Roboto',
+					'Helvetica Neue',
+					'Arial',
+					'sans-serif'
+				]
+			}
+		}
+	},
+	plugins: [forms, typography]
+} satisfies Config;
