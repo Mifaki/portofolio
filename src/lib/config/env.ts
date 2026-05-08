96d7d77f1b9c2fb108c7290c3d75c5da742cce8c
Mifaki <agustianto.d19@gmail.com>
2026-05-08 21:14:36 +0700

    feat: add env config

---
+++ src/lib/config/env.ts
@@ -0,0 +1,5 @@
+import { PUBLIC_API_BASE_URL } from '$env/static/public';
+
+export const ENV = {
+	apiBaseUrl: PUBLIC_API_BASE_URL
+};
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,8 +1,9 @@
+import { ENV } from '$lib/config/env';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ fetch }) => {
 
-  const res = await fetch('https://porto-api.ahmad-faiz.my.id/projects');
+  const res = await fetch(`${ENV.apiBaseUrl}/projects`);
 
   if (!res.ok) {
     throw new Error('Failed to fetch projects');
